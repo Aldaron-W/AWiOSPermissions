dec88c8429087062c3191a8cc9231441d6b26daa
Aldaron <aldaronwang@gmail.com>
2016-06-28 11:47:24 +0800

    Initial Commit

---
+++ AWiOSPermissions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AWiOSPermissions
+//
+//  Created by Aldaron on 16/6/28.
+//  Copyright © 2016年 Aldaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AWiOSPermissions/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AWiOSPermissions
+//
+//  Created by Aldaron on 16/6/28.
+//  Copyright © 2016年 Aldaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
